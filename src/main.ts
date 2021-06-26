0849a28a60a2ba427a9b2a36ce6062c7818220dc
Satont <satontworldwide@gmail.com>
2021-06-26 13:44:49 +0300

    Add temporary log

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,7 @@ const app = express();
 
 app.post('/', function (req, res) {
   if (process.env.WEBHOOK_SECRET !== req.headers.WEBHOOK_SECRET) {
+    console.log(req.headers.WEBHOOK_SECRET);
     return res.status(400).send('Wrong secret');
   }
 
